9cd6d6529e24ecf00b33b7167d6abcf78b666308
Eduardo Roque Viera Santana <eduardovieraaa@gmail.com>
2026-04-06 21:55:08 +0100

    Update base path for Vite configuration

--- miapp/vite.config.js
+++ miapp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/eduviera1407.github.io/home/',
+  base: '/eduardo-viera-portfolio/',
   plugins: [react()],
 })
