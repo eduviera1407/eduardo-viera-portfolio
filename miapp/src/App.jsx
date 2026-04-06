02571a694f376b5fd896e65a207913474f02bcaa
Eduardo Roque Viera Santana <eduardovieraaa@gmail.com>
2026-04-06 21:55:50 +0100

    Refactor Router and improve route handling

--- miapp/src/App.jsx
+++ miapp/src/App.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/Home'; // Página de inicio
+import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import Proyectos from './pages/Proyectos';
-import './styles/App.css'
+import './styles/App.css';
+
 const App = () => {
   return (
-    <Router>
-     
-
+    <Router basename="/eduardo-viera-portfolio">
       <Routes>
-        {/* Ruta que redirige al Home cuando se carga localhost */}
-        <Route path="/" element={<Navigate to="/home" />} />
-
-        {/* Otras rutas */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
-         <Route path="*" element={<ErrorPage />} />
-         <Route path="/proyectos" element={<Proyectos />} />
-
+        <Route path="/proyectos" element={<Proyectos />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
